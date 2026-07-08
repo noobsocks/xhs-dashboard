efce9f0432988a82c855cf850661701295f8e6e4
noobsocks <noobsocks@naver.com>
2026-07-08 10:06:48 +0900

    update

---
+++ socials.js
@@ -0,0 +1,570 @@
+window.XHS_SOCIALS = {
+ "692033dd0000000037009a4e": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/dooooi_lee/"
+ },
+ "68b5003e000000001901df25": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/oljbll_"
+ },
+ "5dd0e27d0000000001002a82": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hwajung95"
+ },
+ "690b036c0000000037006730": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/jxngbinn"
+ },
+ "67f63ee2000000000d0083c3": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/im__barbie_"
+ },
+ "6927eb00000000003702c94d": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/leerian__/"
+ },
+ "66377c41000000000700648e": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/errdayoung"
+ },
+ "68c8f1bc00000000190101cd": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/heejae_1023"
+ },
+ "68c8df9f0000000019016ada": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/lylakong01"
+ },
+ "662b5afc0000000003033900": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/romi.fit"
+ },
+ "6687f1b6000000000303050a": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/forever____25/"
+ },
+ "69099b5f00000000370076e3": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/soondoll_22/"
+ },
+ "6825aa11000000000e01c968": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/joowondae/"
+ },
+ "68a527a40000000019013060": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/jujuorzo"
+ },
+ "68a4017f000000001900d24f": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/minsunmaro"
+ },
+ "68ddf09c000000003702ee47": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/minggyu._"
+ },
+ "68497b42000000001b01a912": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_ruby_iii"
+ },
+ "63ef8fc8000000001001feb6": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yourownbellee/"
+ },
+ "68937f150000000028018792": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hyeran._.yang"
+ },
+ "6946242b000000000300e5f0": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/helstagram__s2"
+ },
+ "68e094250000000037030228": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hiimyoursue?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D"
+ },
+ "68c9589b000000002101fb59": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/ryan.joojonghyuk"
+ },
+ "6902faa20000000037005081": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/ahjkm"
+ },
+ "688247b9000000001d00bf72": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/luvxxmoon/"
+ },
+ "6900661a0000000037001ff6": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/p.sevely"
+ },
+ "6839ee53000000001d00a933": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/uye0n__?igsh=MXdlYW5kcHpjemo4bg%3D%3D&utm_source=qr"
+ },
+ "68d25f30000000002102b072": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/n03yh6d_6"
+ },
+ "5c7dfdef000000001603fc84": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_or.xom"
+ },
+ "694d0f240000000026039804": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/sunghoon1983"
+ },
+ "68235163000000000e01ef19": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@jaymeeforyou"
+ },
+ "61c5acde00000000100088be": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/bba_na_na/"
+ },
+ "6858f75f000000001b021e9e": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_xx__hye"
+ },
+ "6953b5620000000026039d68": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@%EC%95%84%EC%8A%A4%ED%84%B0%EB%AE%A4%EC%A7%81_ASTERMUSIC"
+ },
+ "695faa24000000003700bed1": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@yjoo_oh"
+ },
+ "69c5492a0000000026039cae": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@EatwithBoki/videos"
+ },
+ "69771014000000002603b758": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@%EA%B0%80%EC%9D%84fall0715/videos"
+ },
+ "686ce2ec000000001d008ef3": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/lovely_._v?igsh=YTN6NnphcWI5bzBm"
+ },
+ "68d6eac900000000210278b1": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/59.5994"
+ },
+ "691c74cc000000003702d38a": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/taxxrii"
+ },
+ "6934e4a300000000370016d6": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@Blue.D"
+ },
+ "6234b0a9000000001000e009": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@ya.stacey"
+ },
+ "695d1aa40000000037008c5a": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@chaeyeong._.e"
+ },
+ "69bb83400000000026038ed5": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@dlibfactory"
+ },
+ "673d8e72000000001d02e298": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/bella_w.young/"
+ },
+ "69cb4e010000000026002eee": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@GGILGGILMARKET"
+ },
+ "69579209000000002603a53c": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/ri_yeeeeee/"
+ },
+ "69d70fef00000000260032c7": {
+  "platform": "youtube",
+  "url": "https://youtube.com/channel/UCwfbSsZ7_sccxcibe60mWlQ?si=LjXl3rTR-44mMwyQ"
+ },
+ "6732b6cb000000001c0184ae": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/lynzzzzang/"
+ },
+ "692ae52c0000000003013e5a": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/baexeongyeon"
+ },
+ "69b819e60000000026001c12": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@noeunsol0409"
+ },
+ "69d492a40000000026002b3f": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/bk_kimm"
+ },
+ "69ef2c70000000000d034c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/jangeunhong"
+ },
+ "69ef341f000000000d035c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yesyul_"
+ },
+ "69f067a4000000000d035c02": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/xxkyu_"
+ },
+ "6774e370000000001801fdde": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/nahyun_pilateshd/"
+ },
+ "69f070d2000000000d035c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/xeoeons/"
+ },
+ "69ef2ac0000000000d035c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/lylekimm"
+ },
+ "64a7b123000000001c02adff": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hae.soll2/"
+ },
+ "69f301bd000000000d034c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/han_rin_roo/"
+ },
+ "69f344f5000000000d034c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yea_h_oo?igsh=MTYyOHZic29odjhweA=="
+ },
+ "69f3405c000000000d035c01": {
+  "platform": "youtube",
+  "url": "https://youtube.com/@sceneworld-v2r?si=YFXDJlWf0UYK72Fb"
+ },
+ "69f341cc000000000d035c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/daroyoon?igsh=dDU2aHY2eTd4bjV6"
+ },
+ "69f3423d000000000d035c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/jjung.a__?igsh=M3IxYjNhODRmeG03"
+ },
+ "69f2ea7f000000000d035c03": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@nyankkung"
+ },
+ "62786bff000000002102b934": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/aquafuckingweird/"
+ },
+ "681444b2000000000e01d94d": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/holycow.co.kr?igsh=ZWRhY3N1aTFxcGF3"
+ },
+ "6a041b38000000000d034c02": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/juhee__ss/"
+ },
+ "67f8cbd7000000000e01d436": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/zoo.yeol?igsh=MWdocm9lanR5aXd4aQ%3D%3D&utm_source=qr"
+ },
+ "67f7b5b5000000000e0104a8": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/__me__ne?igsh=N3pjdTBrZXFtN2k4"
+ },
+ "69708797000000002603a6cd": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/8_jjini/"
+ },
+ "65f571b2000000000600cb6f": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/jungongju9/"
+ },
+ "6a0f92d7000000000d035c02": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@Beast_Lee__"
+ },
+ "653239f0000000002a01b285": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/onfinitalin?igsh=NmkwbWtqNzRza2dz"
+ },
+ "593d842650c4b4642f0c382a": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/gezi____/"
+ },
+ "66bb3184000000001d021af0": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/oanuc_/"
+ },
+ "67e0d855000000000a03f911": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@%EB%B0%94%EB%94%94101"
+ },
+ "6814e211000000000d00a01f": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/sk8soxer/"
+ },
+ "6a153f3e000000000d035c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hellopapa11/"
+ },
+ "6a165520000000000d034c00": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@Highfiction.official"
+ },
+ "6859418d000000001b019118": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@ravely1"
+ },
+ "5fcce992000000000101e48c": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hyuniverse___?igsh=a2cyamlkczR6YjVv"
+ },
+ "68b0afe400000000190122d9": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hajeong_08?igsh=bGg3ZHllMTV6NGt6"
+ },
+ "5d0264ce00000000160368a3": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yao_feilin?igsh=MWttaG1hbmFqYnk2ZA%3D%3D&utm_source=qr"
+ },
+ "6a19758a000000000d034c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/kkkiiitttttty?igsh=MWZ1amJlZHhtdjQxcA=="
+ },
+ "69b1a63200000000260016ea": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@ezy_zzang?_r=1&_t=ZS-96lQPc3e46X"
+ },
+ "6a1976d1000000000d034c02": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@youki_1223?_r=1&_t=ZS-96lQavImfab"
+ },
+ "69e61bb1000000002603b9a8": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/navi_yw?igsh=OTN6dTltNWQybmky"
+ },
+ "6a197737000000000d034c03": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_im_ziyu?igsh=cWdkc3hmYTBjZDVp&utm_source=qr"
+ },
+ "6a19117f000000000d034c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/riwonsos?igsh=aG1mNGczeHhkYWo2"
+ },
+ "6a196b95000000000d035c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/mnijongmin?igsh=anAwZmpycmwzeTM4&utm_source=qr"
+ },
+ "6a197f48000000000d035c02": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/seulyena_baek?igsh=MWM0MDZyYXF1eWhpYw%3D%3D&utm_source=qr"
+ },
+ "60c4adc5000000000100032f": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_lucasss02?igsh=MWZzOW1odTVoNGEzMA=="
+ },
+ "6a19825e000000000d034c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hhhyangggg?igsh=cmZ2aDNobTkyNGhs&utm_source=qr"
+ },
+ "645cebc100000000290131c8": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yuchen_01?igsh=OXp4czZrZGhlZnZ1"
+ },
+ "69a2c0430000000026000d57": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hoxvlr?igsh=MXhzZWg0aXRiM3JhNQ=="
+ },
+ "676a8b310000000018014fa4": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hier_r_oo?igsh=d3JrcGxwMXpyMmlu"
+ },
+ "6826ab2a000000000d0080b7": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/dr_hyonjin?igsh=MXEzYmlxZGoyczhjag=="
+ },
+ "6a0be8f3000000000d034c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/seoeeozee?igsh=NHg0cHB5a2FlemY5&utm_source=qr"
+ },
+ "5cad85af00000000100382eb": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/pang_iiin?igsh=MTJtbHU0aWJjdHd5cg=="
+ },
+ "6a1991ad000000000d034c02": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/gimu2?igsh=NTVwNWNpZ3JlbHR2"
+ },
+ "68249e64000000000e01d53f": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yangwoo_kwon"
+ },
+ "6a1991e2000000000d034c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/lee.um?igsh=eXlkMXhhN2d2ZTlu&utm_source=qr"
+ },
+ "61aff64e000000001000f9ba": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/selin.egmn?igsh=NWczZXNiYzAwYmRv"
+ },
+ "6045e4e1000000000101d66b": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/zi.fan_?igsh=MTY4ZGk3Y2QxZ3dybg=="
+ },
+ "6a1994b5000000000d034c01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/seouler.foodies?igsh=ajVsbWF1bnFoYWY5"
+ },
+ "6a19a0d5000000000d035c00": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@j_uu_n.i?_r=1&_t=ZS-96ldsTDBbds"
+ },
+ "6a19a46a000000000d035c01": {
+  "platform": "instagram",
+  "url": "https://instagram.com/bz_teddygrade?igsh=MWRnZ3E4cjYwMHlxcg=="
+ },
+ "69bbef710000000026003b74": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/joon_hhhhhh?igsh=MTBucm9sNmdxOWg5cw%3D%3D&utm_source=qr"
+ },
+ "68526a75000000001d0089f2": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/shabet_serri?igsh=MWNyY3p6d3dvYzIycw=="
+ },
+ "627219e2000000002102279c": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/austinkangg?igsh=enF3ZWxndXRiNzF5"
+ },
+ "698aefc60000000026003ba2": {
+  "platform": "youtube",
+  "url": "https://youtube.com/@jaykeeout?si=LWncafe_ACI62wXz"
+ },
+ "6a22649c000000000d034c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/ballet_jy?igsh=enNwbjZ2bGFqY2M3"
+ },
+ "672b8011000000001d02cb93": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/goxnniee/"
+ },
+ "6a1fd544000000000d034c03": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/sux_eon?igsh=Y210djl1cGh1cWRt&utm_source=qr"
+ },
+ "62cfe283000000000200089c": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/cowbaxa?igsh=MTE0ODNobHU3dHFhOA=="
+ },
+ "6854f245000000001d009070": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/christiandiorboy?igsh=cTA5NWV2YXNxOGFi"
+ },
+ "6a2faba5000000000e03bc00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yoo__si"
+ },
+ "6a2a62c7000000000e03bc01": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/na_yuraya"
+ },
+ "66f78c2f000000001d033ac2": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/07_27___/"
+ },
+ "69beb6bf000000002603b769": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/by.jaeyee/"
+ },
+ "69bb56fd00000000260015d7": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@team1llusion"
+ },
+ "6a0b9603000000000d034c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/2an.2ne/"
+ },
+ "663465150000000007007c70": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@%EC%91%A4%EB%A1%9D%EC%9D%B4SSOLOG"
+ },
+ "69314eb5000000003702b81d": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/min_j_k003?igsh=Mjk1a3BxZnpleml1"
+ },
+ "693c356e000000003700b8c5": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@sojung008251?_r=1&_t=ZS-97KaAnh0Hum"
+ },
+ "6a1ce3f7000000000d035c00": {
+  "platform": "tiktok",
+  "url": "https://www.tiktok.com/@25ksy?_r=1&_t=ZS-97RVohR3P64"
+ },
+ "6a3a1825000000000e03b800": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/singjoo_mp3?igsh=MWZiZ3FpMDhvcTVrYw=="
+ },
+ "69d081a30000000026003420": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/moma_1219?igsh=emRucmZhdjFwaDR1"
+ },
+ "641737050000000012012fdc": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/3.5kg_/"
+ },
+ "6a210f64000000000d035c00": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@JJsalondefit"
+ },
+ "5d34816d00000000110281a9": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/jiang1551/"
+ },
+ "65f8f62b000000000600c677": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/chillin2bmi/"
+ },
+ "6a2a5be6000000000d034c01": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@%EA%B6%8C%EA%B0%90%EA%B0%81"
+ },
+ "6a210f3c000000000d034c01": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@enkangbro"
+ },
+ "6500ef6700000000060310a3": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/hwangdonghee_/"
+ },
+ "5f084b2e000000000101fbe2": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/y00_boogie/"
+ },
+ "6a3cc471000000000d034c01": {
+  "platform": "youtube",
+  "url": "https://www.youtube.com/@geekble_kr"
+ },
+ "696074f400000000370330ee": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/apricotyeon/"
+ },
+ "66ded190000000001d031339": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/yeominii?igsh=dHk3d291MWNnbG5l&utm_source=qr"
+ },
+ "69b581e8000000002603a711": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_chu.bee_?igsh=MTM0Y2dzcmhucDFzdg%3D%3D&utm_source=qr"
+ },
+ "6a449657000000000d035c02": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/_wonja?igsh=MTlqamFheTh4bHQ2bA=="
+ },
+ "6a45d840000000000d034c00": {
+  "platform": "instagram",
+  "url": "https://www.instagram.com/magicianbenson/"
+ }
+};
